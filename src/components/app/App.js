00012c579ef0141ae8eb86e761257f8df7ec3a7f
Nastya <shevchenko.nasta13@gmail.com>
2022-09-06 13:32:29 +0300

    react router 6

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import {MainPage, ComicsPage} from "../pages"
 const  App = () => {
@@ -10,16 +10,11 @@ const  App = () => {
                 <div className="app">
                     <AppHeader/>
                     <main>
-                        <Switch>
-                        <Route exact path="/">
-                            <MainPage/>
-                        </Route>
+                        <Routes>
+                        <Route path="/" element = {<MainPage/>}/>
 
-
-                        <Route exact path="/comics">
-                            <ComicsPage/>
-                        </Route>
-                        </Switch>
+                        <Route path="/comics" element={<ComicsPage/>}/>
+                        </Routes>
                     </main>
                 </div>
             </Router>
